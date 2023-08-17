29d4aa9e4350e97382056a6a98b614934f788446
yinjg <yinjg1997@163.com>
2023-08-17 18:35:11 +0800

    fix: eslint

--- 50-tailwindcss/tailwind.config.js
+++ 50-tailwindcss/tailwind.config.js
@@ -1,2 +1,6 @@
 /** @type {import(tailwindcss).Config} */
-export default {content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}",], theme: {extend: {},}, plugins: [],}
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
+}
